925def44d13a799f5889435aa6fc0a1280988d36
family920826 <3127223582@qq.com>
2017-05-16 10:04:07 +0800

    第一次添加

---
+++ TeachSys/TeachSys/Controllers/BaseDataDicsController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TeachSys.Controllers
+{
+    public class BaseDataDicsController : Controller
+    {
+        //
+        // GET: /BaseDataDics/
+        Models.TeachDBEntities1 tdb = new Models.TeachDBEntities1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetDatabase()
+        {
+            try
+            {
+
+                var da = (from d in tdb.BaseDataDics
+                          select new { id = d.ID, name = d.Name ,d.Status });
+                return Json(da, JsonRequestBehavior.AllowGet);
+            }
+            catch
+            {
+                return Content("error");
+            }
+        }
+        public ActionResult AddDatabase(Models.BaseDataDics b)
+        {
+            try
+            {
+                tdb.BaseDataDics.Add(b);
+                tdb.SaveChanges();
+                return Content("ok");
+            }
+            catch
+            {
+                return Content("error");
+            }
+        }
+        public ActionResult EditDatabase(int typeno, string name, int id,int status)
+        {
+            try
+            {
+                var b= tdb.BaseDataDics.First(t => t.ID == id);
+                b.Name = name;
+                b.TypeNo = typeno;
+                b.Status = status;
+                tdb.SaveChanges();
+                return Content("ok");
+
+            }
+            catch
+            {
+                return Content("error");
+            }
+        }
+        public ActionResult DeleteDatabase(int id)
+        {
+            try
+            {
+                var no = tdb.BaseDataDics.First(t =>t.ID ==id);
+                tdb.BaseDataDics.Remove(no);
+                tdb.SaveChanges();
+                return Content("ok");
+            }
+            catch
+            {
+                return Content("error");
+            }
+        }
+        public ActionResult GettypeID(int typeno)
+        {
+            var classes = from c in tdb.BaseDataDics
+
+
+                          select new
+                          {
+                              ID = c.ID,
+                              Name = c.Name,
+                              Index = c.IndexNo,
+                              typeno = c.TypeNo,
+                              Status = c.Status
+                          };
+            if (typeno != -1)
+            {
+                classes = classes.Where(t => t.typeno == typeno);
+            }
+
+            return Json(classes, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+
+
+        
+
+    }
+}
